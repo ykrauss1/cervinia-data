374477f0be0aa476c67c7410167897b1e52bb683
ykrauss1 <ykrauss1@gmail.com>
2026-02-16 08:27:10 +0200

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
 
 (async () => {
   try {
